ae083dd7a9462ab3732b415f2c7128b3fba4f039
sw22m <142818407+sw22m@users.noreply.github.com>
2023-11-16 20:46:12 +0000

    test

--- lib/config.dart
+++ lib/config.dart
@@ -2,7 +2,7 @@
 String get apiHost {
   bool isProd = const bool.fromEnvironment('dart.vm.product');
   if (isProd) {
-    return String.fromEnvironment("API_HOST");
+    return String.fromEnvironment("API_HOST", defaultValue: "http://localhost:8080");
   }
   return String.fromEnvironment("API_HOST", defaultValue: "http://localhost:8080");
 }
